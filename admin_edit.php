5458b72c075035c3bd352a53ed99664be205e452
tinmarecic6 <tin.marecic@gmailcom>
2020-06-11 22:14:26 +0200

    admin edit now writes to db(not tested)

--- admin_edit.php
+++ admin_edit.php
@@ -38,49 +38,41 @@
     if(isset($_GET['resid']) && $_GET['resid']!='' ){
         $resid = $_GET['resid'];
     }
-
-/*Cisto da znas tino, POST hvata sva polja iz form tagova po name-u ne po id-u(podsjetnik) */
     if(isset($_POST['username']) && $_POST['username']!= ''){
         $sql_update_username = 'UPDATE `user` SET `Username`="'.$_POST['username'].'" WHERE `User_ID` = '.$userid.';';
-        echo $sql_update_username;
-        $sql_update_username='';
-        echo 'izvrsavam query';
+        $conn->query($sql_update_username);
+        #header("Location: admin_profile_page.php");
     };
     if(isset($_POST['firstname']) && $_POST['firstname']!= ''){
         $sql_update_first_name = 'UPDATE `user` SET `First_name`="'.$_POST['firstname'].'" WHERE `User_ID` = '.$userid.';';
-        echo $sql_update_first_name;
-        $sql_update_first_name='';
-        echo 'izvrsavam query';
+        $conn->query($sql_update_first_name);
+        #header("Location: admin_profile_page.php");
+
     };
     if(isset($_POST['lastname']) && $_POST['lastname']!= ''){
         $sql_update_last_name = 'UPDATE `user` SET `Last_name`="'.$_POST['lastname'].'" WHERE `User_ID` = '.$userid.';';
-        echo $sql_update_last_name;
-        $sql_update_last_name='';
-        echo 'izvrsavam query';
+        $conn->query($sql_update_last_name);
+        #header("Location: admin_profile_page.php");
     };
     if(isset($_POST['user_image']) && $_POST['user_image']!= ''){
         $sql_update_user_image = 'UPDATE `user` SET `User_image`="'.$_POST['user_image'].'" WHERE `User_ID` = '.$userid.';';
-        echo $sql_update_user_image;
-        $sql_update_user_image='';
-        echo 'izvrsavam query';
+        #not finisher, requires file transfer
+        #$conn->query($sql_update_user_image);
+        #header("Location: admin_profile_page.php");
     };
     if(isset($_POST['typeid']) && $_POST['typeid']!= ''){
         $sql_update_type_id = 'UPDATE `user` SET `Type_id`="'.$_POST['typeid'].'" WHERE `User_ID` = '.$userid.';';
-        echo $sql_update_type_id;
-        $sql_update_type_id='';
-        echo 'izvrsavam query';
+        $conn->query($sql_update_type_id);
+        #header("Location: admin_profile_page.php");
     };
     if(isset($_POST['dob']) && $_POST['dob']!= ''){
         $sql_update_dob = 'UPDATE `user` SET `Date_of_birth`="'.$_POST['dob'].'" WHERE `User_ID` = '.$userid.';';
-        echo $sql_update_dob;
-        $sql_update_dob='';
-        echo 'izvrsavam query';
+        $conn->query($sql_update_dob);
+        #header("Location: admin_profile_page.php");
     };
     if(isset($_POST['password']) && $_POST['password']!= ''){
         $sql_update_password = 'UPDATE `user` SET `Password`="'.$_POST['password'].'" WHERE `User_ID` = '.$userid.';';
-        echo $sql_update_password;
-        $sql_update_password='';
-        echo 'izvrsavam query';
+        $conn->query($sql_update_password);
     };
 
 /* OBJEKTI */
@@ -90,14 +82,11 @@
     
     if(isset($_POST['object_name']) && $_POST['object_name']!= ''){
         $sql_update_object_name = 'UPDATE `object` SET `Object_name`="'.$_POST['object_name'].'" WHERE `Object_ID` = '.$objid.';';
-        $sql_update_object_name='';
-        echo 'izvrsavam query';
+        $conn->query($sql_update_object_name);
     };
     if(isset($_POST['price']) && $_POST['price']!= ''){
         $sql_update_price = 'UPDATE `object` SET `Price`="'.$_POST['price'].'" WHERE `Object_ID` = '.$objid.';';
-        echo $sql_update_price;
-        $sql_update_price='';
-        echo 'izvrsavam query';
+        $conn->query($sql_update_price);
 };
 
 /* REZERVACIJE */
@@ -106,27 +95,19 @@
     }
     if(isset($_POST['date_from']) && $_POST['date_from']!= ''){
         $sql_update_date_from = 'UPDATE `reservation` SET `Date_from`="'.$_POST['date_from'].'" WHERE `Reservation_ID` = '.$resid.';';
-        echo $sql_update_date_from;
-        $sql_update_date_from='';
-        echo 'izvrsavam query';
+        $conn->query($sql_update_date_from);
     };
     if(isset($_POST['date_to']) && $_POST['date_to']!= ''){
         $sql_update_date_to = 'UPDATE `reservation` SET `Date_to`="'.$_POST['date_to'].'" WHERE `Reservation_ID` = '.$resid.';';
-        echo $sql_update_date_to;
-        $sql_update_date_to='';
-        echo 'izvrsavam query';
+        $conn->query($sql_update_date_to);
     };
     if(isset($_POST['status']) && $_POST['status']!= ''){
         $sql_update_status = 'UPDATE `reservation` SET `Status`="'.$_POST['status'].'" WHERE `Reservation_ID` = '.$resid.';';
-        echo $sql_update_status;
-        $sql_update_status='';
-        echo 'izvrsavam query';
+        $conn->query($sql_update_status);
     };
     if(isset($_POST['confirmed']) && $_POST['confirmed']!= ''){
         $sql_update_confirmed = 'UPDATE `reservation` SET `Confirmed`="'.$_POST['confirmed'].'" WHERE `Reservation_ID` = '.$resid.';';
-        echo $sql_update_confirmed;
-        $sql_update_confirmed='';
-        echo 'izvrsavam query';
+        $conn->query($sql_update_confirmed);
     };
 
 ?>
