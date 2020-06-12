1b430c7bf407b178c0360d03b3c8923db95dfead
tinomargan <tinomargan@gmail.com>
2020-06-12 16:52:15 +0200

    uredio kod da ljepse izgleda

--- profile_redirect.php
+++ profile_redirect.php
@@ -1,34 +1,14 @@
 <?php
-require_once('scripts/db.php');
-$conn = db();
 
-/*Login backend*/
-/*
-$username = $_POST['username'];
-$pass = $_POST['pass'];
-$sql_user = 'SELECT * from `user` where Username="'.$username.'" and Password="'.$pass.'";';
-$result = $conn->query($sql_user);
-*/
-session_start();
-/*
-$_SESSION['error_mess'] = '<div class="alert alert-danger alert-dismissable fade show" role="alert">
-Krivi Username ili password
-<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-<span aria-hidden="true">&times;</span>
-</button></div>';
-if($result && $result->num_rows==1){
-    $row = $result->fetch_assoc();
-    $_SESSION['username']=$username;
-    $_SESSION['Type_ID']=$row['Type_ID'];
-    $_SESSION['User_ID'] = $row['User_ID'];*/
+    require_once('scripts/db.php');
+    $conn = db();
+
+    session_start();
+
     switch($_SESSION['Type_ID']){
         case 2: header("Location: host_profile_page.php");break;
         case 3: header("Location: user_profile_page.php");break;
         default : header("Location: homepage.php");break;
-    }/*
-}
-else{
-    //header("Location: index.php"); 
-}*/
+    }
 
 ?>
