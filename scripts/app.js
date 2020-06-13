2cf26c3c2b64b93368d48b6164b5cfbe6864873b
tinomargan <tinomargan@gmail.com>
2020-06-13 17:09:42 +0200

    self explanatory

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+//window.onload = function() {
+    var startdate = document.getElementById("datefrom").value;
+    var enddate = document.getElementById("dateto").value;
+    
+    console.log(startdate);
+    //enddate = startdate.stepUp(1);
+//};
