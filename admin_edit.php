62c9b7f5cd199919d4ecf4e8eb05659e1fc9b24e
tinmarecic6 <tin.marecic@gmailcom>
2020-06-15 13:06:26 +0200

    new datepicker

--- admin_edit.php
+++ admin_edit.php
@@ -4,13 +4,21 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <!-- Bootstrap CSS -->
     
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="Style/user_page.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
     <title>Edit info</title>
+    <!--Datepicker-->
+    <script type="text/javascript" src="datepicker/moment.js"></script>
+	<script type="text/javascript" src="datepicker/daterangepicker.js"></script>
+	<link rel="stylesheet" type="text/css" href="datepicker/daterangepicker-bs3.css" />
+	<link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+	<script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
   </head>
 
   <!--user type test-->
@@ -125,14 +133,14 @@
 
 /* REZERVACIJE */
 
-    if(isset($_POST['datefrom']) && $_POST['datefrom']!= ''){
-        $sql_update_datefrom = 'UPDATE `reservation` SET `Date_from`="'.$_POST['datefrom'].'" WHERE `Reservation_ID` = '.$resid.';';
-        $conn->query($sql_update_datefrom);
-        $insertedadmin = 1;
-    };
-    if(isset($_POST['dateto']) && $_POST['dateto']!= ''){
-        $sql_update_dateto = 'UPDATE `reservation` SET `Date_to`="'.$_POST['dateto'].'" WHERE `Reservation_ID` = '.$resid.';';
-        $conn->query($sql_update_dateto);
+    if(isset($_POST['reservation']) && $_POST['reservation']!= ''){
+        $res = explode(' - ',$_POST['reservation']);
+        $datefrom = strtotime($res[0]);
+        $date_from_conv = date('Y-m-d',$datefrom);
+        $dateto = strtotime($res[1]);
+        $date_to_conv = date('Y-m-d',$dateto);
+        $sql_date_res = 'UPDATE `reservation` set Date_from = "'.$date_from_conv.'", Date_to = "'.$date_to_conv.'" WHERE `Reservation_ID` = '.$resid.';';
+        $conn->query($sql_date_res);
         $insertedadmin = 1;
     };
     if(isset($_POST['status']) && $_POST['status']!= ''){
@@ -258,12 +266,16 @@
             <form class="container mt-5 pt-4" name="editres" method="POST" action="?resid=<?php echo $resid; ?>">
                 <h3 class="mb-5">Change reservation info:</h3>
                 <div class="form-group">
-                <label for="datefrom">Date from:</label>
-                    <input type="date" class="form-control" name="datefrom" id="datefrom">
-                </div>
-                <div class="form-group">
-                <label for="dateto">Date to:</label>
-                    <input type="date" class="form-control" name="dateto" id="dateto">
+                <fieldset>
+                      <div class="control-group">
+                        <div class="controls">
+                        <div class="input-prepend input-group">
+                          <span class="add-on input-group-addon"><i class="glyphicon glyphicon-calendar fa fa-calendar"></i></span>
+                          <input required type="text" style="width: 200px" name="reservation" id="reservation" class="form-control" autocomplete="off"  placeholder="Razdoblje boravka" /> 
+                        </div>
+                        </div>
+                      </div>
+                    </fieldset>
                 </div>
                 <div class="form-check">
                     <input type="checkbox" class="form-check-input" name="status" value="1">
@@ -282,6 +294,35 @@
             </form>
         <?php endif;?>
 
+<!--Datepicker-->
+<script type="text/javascript">
+	 		var today = new Date();
+            var tomorow = new Date();
+			var dd = today.getDate();
+			var mm = today.getMonth() + 1; //January is 0!
+			var yyyy = today.getFullYear();
 
+			if (dd < 10) {
+			  dd = '0' + dd;
+			}
+
+			if (mm < 10) {
+			  mm = '0' + mm;
+			}
+			var sd = dd + 1;
+			today = dd + '-' + mm + '-' + yyyy;
+			tomorow = sd + '-' + mm + '-' + yyyy;	
+			
+			$('#reservation').daterangepicker(
+			  { 
+			  	format: "DD-MM-YYYY",
+			  	startDate:today,
+			  	endDate:tomorow,
+			    minDate: today,
+			  },
+			  function(start, end, label) {
+			  }
+			);
+	</script>
 </body>
 </html>
